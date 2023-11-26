5f34dfd74379e09e464564c91ffcaea04d624dfb
Zaid Shaikh <s.zaidshaikh99@gmail.com>
2023-11-26 20:54:24 +0530

    next commit

---
+++ src/Person.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class Person extends Component {
+  render() {
+    // return (
+    //   <div className="App-Header">
+    //     <i>
+    //       <h1>Hello GFG</h1>
+    //     </i>
+    //   </div>
+    // );
+    return React.createElement(
+      "div",
+      { class: "App-header" },
+      React.createElement(
+        "i",
+        null,
+        React.createElement("h1", null, "Hello GFG")
+      )
+    );
+  }
+}
+
+export default Person;
